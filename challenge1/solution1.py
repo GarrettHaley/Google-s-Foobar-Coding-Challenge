5941dbfffe027aa06056972b2592345f2fb078f8
GarrettHaley <haleyg@oregonstate.edu>
2018-03-27 18:30:08 -0700

    Folder Changes

--- challenge1/solution1.py
+++
@@ -1,8 +0,0 @@
-def answer(s):
-    solutes = 0
-    for i in range(0,len(s)):
-        if s[i] == ">":
-            for char in s[i:]:
-                if char == "<":
-                    solutes += 2
-    return solutes
